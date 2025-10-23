efccc32e5a97ebe64d17ca263c42b65f276eb655
Michael van Niekerk <mike@agri-io.co.za>
2025-10-23 08:30:24 +0200

    F32Response

--- src/shared_responses.rs
+++ src/shared_responses.rs
@@ -1,5 +1,7 @@
-use crate::NumberResponse;
+use crate::{NumberResponse, VALUES_SERIALIZE_OPTIONS};
 use atat_derive::{AtatLen, AtatResp};
+#[cfg(feature = "debug")]
+use defmt::error;
 use serde::Serialize;
 use serde_at::HexStr;
 
@@ -60,14 +62,104 @@ impl From<u32> for U32Response {
     }
 }
 
-#[derive(Debug, Clone, AtatResp, PartialEq, Serialize, AtatLen)]
+#[derive(Debug, Clone, PartialEq, Serialize, AtatResp, AtatLen)]
 pub struct F32Response {
-    pub value: f32,
+    pub positive: bool,
+    pub unit: u16,
+    pub decimal: u8,
+    pub decimal_len: u8,
+    pub exponent: i8,
 }
-impl NumberResponse for F32Response {}
 
 impl From<f32> for F32Response {
     fn from(value: f32) -> Self {
-        F32Response { value }
+        let positive = value > 0.0;
+        match serde_at::to_string::<_, 42>(&value, "", VALUES_SERIALIZE_OPTIONS) {
+            Ok(s) => {
+                let mut s = s.split('e');
+                let val = s.next().unwrap_or("0.0");
+                let exponent = s.next().unwrap_or("0");
+                let exponent = exponent.parse::<i8>().unwrap_or(0);
+                let mut val = val.split('.');
+                let unit = val.next().unwrap_or("0");
+                let unit = if positive { unit } else { &unit[1..] };
+                let unit = unit.parse::<u16>().unwrap_or(0);
+                let decimal = val.next().unwrap_or("0");
+                let decimal_len = decimal.len() as u8;
+                let decimal = decimal.parse::<u8>().unwrap_or(0);
+                F32Response {
+                    positive,
+                    unit,
+                    decimal,
+                    decimal_len,
+                    exponent,
+                }
+            }
+            Err(_) => F32Response {
+                positive: false,
+                unit: 0,
+                decimal: 0,
+                decimal_len: 0,
+                exponent: 0,
+            },
+        }
+    }
+}
+
+impl From<&F32Response> for f32 {
+    fn from(value: &F32Response) -> Self {
+        let mut decimal = value.decimal as f32;
+        decimal /= 10u32.pow(value.decimal_len as u32) as f32;
+
+        let mut ret = value.unit as f32 + decimal;
+        if value.exponent < 0 {
+            ret /= 10u32.pow(value.exponent.abs() as u32) as f32;
+        } else {
+            ret *= 10u32.pow(value.exponent as i32 as u32) as f32;
+        }
+        if !value.positive {
+            ret *= -1.0;
+        }
+        ret
+    }
+}
+
+impl NumberResponse for F32Response {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::ToVecBytesResponse;
+    use heapless::String;
+
+    #[test]
+    fn f32_response_parsing() {
+        let b: F32Response = 1.01f32.into();
+        let b = b.to_vec_bytes_response("+abc").unwrap();
+        let s = String::from_utf8(b).unwrap();
+        assert_eq!("+abc: true,1,1,2,0\r\nOK\r\n", s.as_str());
+        let s = "+abc: true,1,1,2,0";
+        let val: F32Response = serde_at::from_str(s).unwrap();
+        let val: f32 = (&val).into();
+        assert_eq!(1.01, val);
+
+        let b: F32Response = 3.102e4f32.into();
+        let b = b.to_vec_bytes_response("+abc").unwrap();
+        let s = String::from_utf8(b).unwrap();
+        assert_eq!("+abc: true,3,102,3,4\r\nOK\r\n", s.as_str());
+        let s = "+abc: true,3,102,3,4";
+        let val: F32Response = serde_at::from_str(s).unwrap();
+        let val: f32 = (&val).into();
+        assert_eq!(31.02e3, val);
+
+        let b = 3.102e4f32 / 100_000_000.00 * -1.0;
+        let b: F32Response = b.into();
+        let b = b.to_vec_bytes_response("+abc").unwrap();
+        let s = String::from_utf8(b).unwrap();
+        assert_eq!("+abc: false,3,102,3,-4\r\nOK\r\n", s.as_str());
+        let s = "+abc: false,3,102,3,-4";
+        let val: F32Response = serde_at::from_str(s).unwrap();
+        let val: f32 = (&val).into();
+        assert_eq!(3.102e4f32 / 100_000_000.00 * -1.0, val);
     }
 }
